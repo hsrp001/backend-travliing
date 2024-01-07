d2c7dd36fcf4ea4c6fce0b43c42487aa7b1c3750
Sonu110 <kumarsonu60067@gmail.com>
2024-01-06 22:55:35 -0800

    sdfsd

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.json())
 app.use('/uploads',express.static(__dirname+'/uploads'))
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://mermorymakers.netlify.app",
     credentials:true
 }))
 
